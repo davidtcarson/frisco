e8df20acc06b26423dd76922619bcb138d23ed3c
Dave <davidtcarson@gmail.com>
2011-07-18 23:02:51 -0400

    CSS reorganization. Temporary markup adjustments via functions file.

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,22 @@
 <!-- No sidebar for you! -->
+<?php do_action( 'bp_before_sidebar' ) ?>
+
+	<?php do_action( 'bp_inside_before_sidebar' ) ?>
+
+	<?php if ( is_user_logged_in() ) : ?>
+
+		<?php do_action( 'bp_before_sidebar_me' ) ?>
+
+		<?php do_action( 'bp_after_sidebar_me' ) ?>
+
+	<?php else : ?>
+
+		<?php do_action( 'bp_before_sidebar_login_form' ) ?>
+	
+		<?php do_action( 'bp_after_sidebar_login_form' ) ?>
+
+	<?php endif; ?>
+
+	<?php do_action( 'bp_inside_after_sidebar' ) ?>
+
+<?php do_action( 'bp_after_sidebar' ) ?>
